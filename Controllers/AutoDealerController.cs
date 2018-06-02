809bc962b4e41006390b47928790e6a0499276a5
Nosforatu <39199818+Nosforatu@users.noreply.github.com>
2018-06-02 13:59:23 +0200

    Implement insert and update services

--- Controllers/AutoDealerController.cs
+++ Controllers/AutoDealerController.cs
@@ -1,11 +1,71 @@
+using AutoTrader.Models;
+using AutoTrader.Models.ViewModels.AutoDealer;
+using AutoTrader.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace AutoTrader.Controllers
 {
     public class AutoDealerController : Controller
     {
-        public IActionResult  Index()
+        public VehicleService vehicleService;
+        public AutoDealerController(VehicleService vehicleService)
         {
+            this.vehicleService = vehicleService;
+        }
+
+        public async Task<IActionResult> Index(IndexViewModel vm)
+        {
+            vm.Vehicles = await vehicleService.GetVehicles().ToListAsync();
+
+            return View(vm);
+        }
+
+        public async Task<IActionResult> InsertVehicle(InsertVehicleViewModel vm)
+        {
+            return View(vm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Insert(InsertVehicleViewModel vm)
+        {
+            // Validation
+            if(!ModelState.IsValid)
+            {
+                vm.Message = ModelState.Values.SelectMany(v => v.Errors).FirstOrDefault().ErrorMessage;
+
+                return RedirectToAction("InsertVehicle", vm);
+            }
+
+            Vehicle vehicle = new Vehicle()
+            {
+                CylinderVariant = vm.CylinderVariant,
+                EngineCapacity = vm.EngineCapacity,
+                Make = vm.Make,
+                Model = vm.Model,
+                Price = vm.Price,
+                TopSpeed = vm.TopSpeed
+            };
+
+            await vehicleService.Insert(vehicle);
+            
+            return RedirectToAction("InsertVehicle",vm);
+        }
+
+        public async Task<IActionResult> UpdateVehicle(Guid VehicleId)
+        {
+            var vehicle = await vehicleService.GetVehicles()
+                .Where(w => w.VehicleId.Equals(VehicleId))
+                .FirstOrDefaultAsync();
+
+            if(vehicle == null)
+            {
+                return RedirectToAction("Index",new IndexViewModel() { Message = "Invalid Vehicle ID" });
+            }
+            
             return View();
         }
     }
---
+++ Models/ViewModels/AutoDealer/IndexViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoTrader.Models.ViewModels.AutoDealer
+{
+    public class IndexViewModel
+    {
+        public List<Vehicle> Vehicles { get; set; }
+        public string SearchCriteria { get; set; }
+        public PaginationOptions GetPaginationOptions { get; set; }
+        public string Message { get; set; }
+    }
+}
