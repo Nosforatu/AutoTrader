bedcd9836d72740d9c697e7586b6df99d10169c3
Nosforatu <39199818+Nosforatu@users.noreply.github.com>
2018-06-04 22:29:43 +0200

    Added charts with view model

---
+++ Controllers/ChartController.cs
@@ -0,0 +1,36 @@
+﻿using AutoTrader.Models.ViewModels.AirPressure;
+using AutoTrader.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoTrader.Controllers
+{
+    public class ChartController : Controller
+    {
+        public VehicleService vehicleService;
+
+        public ChartController(VehicleService vehicleService)
+        {
+            this.vehicleService = vehicleService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult AirPressure()
+        {
+            AirPressureViewModel vm = new AirPressureViewModel();
+            vm.QueryName = "Air Pressure";
+
+
+
+            return new JsonResult(vm);
+        }
+    }
+}
